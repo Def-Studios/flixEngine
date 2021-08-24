326faeebdcf375f926eed849a5859bd9696b5195
Axy <85826200+AxyGitPoggers@users.noreply.github.com>
2021-08-24 17:48:58 +0100

    br

--- new flix project/game.cmd
+++ new flix project/game.cmd
@@ -1,4 +1,3 @@
 @echo off
 title game
-print executing game main.py
 py src/main.py
--- new flix project/src/FxGame.py
+++ new flix project/src/FxGame.py
@@ -8,18 +8,15 @@ import json
 import pypresence
 import os 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-createdgame = pygame.init()
 #game setup noncence
 print(dir_path)
 with open(dir_path + "\gameinfo.json") as gameinfo:
   data = json.load(gameinfo)
 with open(dir_path + "/assets.json") as assetpath:
   assetPaths = json.load(assetpath)
-screen = pygame.display.set_mode((400,400))
-clock = pygame.time.Clock()
-font = pygame.font.SysFont("Arial", 18)
 gameName = data.get('name')
 gameVer = data.get('version')
+init = 0
 class Game:
   with open(dir_path + "\gameinfo.json") as gameinfo:
     data = json.load(gameinfo)
@@ -28,9 +25,9 @@ class Game:
   # ignore evrything from now on
   gameName = data.get('name')
   gameVer = data.get('version')
-  screen = pygame.display.set_mode((400,400))
 
   def initGame(r:float, g:float, b:float, width, hight):
+
       with open(dir_path + "\gameinfo.json") as gameinfo:
         data = json.load(gameinfo)
       with open(dir_path + "/assets.json") as assetpath:
@@ -39,20 +36,18 @@ class Game:
       gameName = data.get('name')
       gameVer = data.get('version')
       # varibles go here..
-      screen = pygame.display.set_mode((400,400))
-      clock = pygame.time.Clock()
-      font = pygame.font.SysFont("Arial", 18)
-      size = width, height = width, hight
-      screen = pygame.display.set_mode(size)
+      pygame.init()
+      screen = pygame.display.set_mode(width, hight)
       color = r, g, b
       screen.fill(color)
       pygame.display.set_caption(gameName)
       print("game " + gameName + " version " + str(gameVer) + " created!")
-      return(pygame)
+      init = 1
 
 
 
 while 1:
+  if init == 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
            print("game " + gameName + " quit")
