b191c0d25de519dab47c46983bae6101a11e9687
Axy <85826200+AxyGitPoggers@users.noreply.github.com>
2021-08-24 18:29:16 +0100

    Revert "fg"
    This reverts commit fff9e9a3b3fb8aa2b1e1626ba54e2228dddc8d32.

--- new flix project/src/main.py
+++ new flix project/src/main.py
@@ -10,9 +10,10 @@ import pypresence
 import os 
 
 class main:
+    game = Game.initGame(0, 0, 0, 640, 480)
     def __init__(self):
-        Game.setWH(640, 480)
-        Game.initGame(0, 0, 0, 640, 480)
+        # when the game starts
+        pass
 
     while 1:
         pass #update
