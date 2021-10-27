6051c159a6ee5c4cb151c105ddfadf478d3e644f
Def <85826200+timeless13GH@users.noreply.github.com>
2021-10-27 15:47:04 +0100

    initial commit

--- new flix project/src/runtime/flix.hx
+++
@@ -1,9 +0,0 @@
-package;
-// set up some annoying stuff
-
-include py;
-py.init();
-
-py.game.init(0,0,0, 234,253);
-py.state.create("src/main.py", (game));
-py.sys.quit();
--- newflix.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set /p id="name: "
-robocopy "new flix project" %id% /E
-cd %id%
-rename game.exe %id%.exe
